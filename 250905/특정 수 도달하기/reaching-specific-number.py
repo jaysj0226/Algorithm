24cbea78964fe7d8111b31016ff27b7beece1a2e
정선재 <95292789+jaysj0226@users.noreply.github.com>
2025-09-05 14:18:51 +0900

    Title: 특정 수 도달하기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250905/특정 수 도달하기/reaching-specific-number.py
@@ -0,0 +1,13 @@
+n = 10
+arr= list(map(int,input().split()))
+
+ans = []
+for i in range(len(arr)):
+    if arr[i] >= 250:
+       break
+    ans.append(arr[i])
+    
+
+sum_ans = sum(ans)
+avg_ans = sum_ans / len(ans)
+print(sum_ans, avg_ans)
