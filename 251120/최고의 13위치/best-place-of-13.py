e98e10d557d3377b89b2a06264c9d9de976fbf0c
정선재 <95292789+jaysj0226@users.noreply.github.com>
2025-11-20 22:33:59 +0900

    Title: 최고의 13위치, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251120/최고의 13위치/best-place-of-13.py
@@ -0,0 +1,14 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+
+def simulate():
+    max_cnt=0
+    for i in range(n):
+        for j in range(n-2):
+            max_cnt = max(max_cnt, grid[i][j] + grid[i][j+1] + grid[i][j+2])
+    return max_cnt
+
+ans = simulate()
+print(ans)
