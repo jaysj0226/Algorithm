8694b364ef9d5f2af64151f19cf3067577b39771
정선재 <95292789+jaysj0226@users.noreply.github.com>
2025-12-24 17:41:11 +0900

    Title: Date to Date, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251224/Date to Date/date-to-date.py
@@ -0,0 +1,21 @@
+m1, d1, m2, d2 = map(int, input().split())
+
+# Please write your code here.
+def num_of_days(m,d):
+    days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    total_days = 0
+
+    for i in range(1,m):
+        total_days += days[i]
+
+    total_days += d
+
+    return total_days
+
+
+total_days = num_of_days(m2,d2) - num_of_days(m1,d1) + 1
+print(total_days)
+        
+
+
+
