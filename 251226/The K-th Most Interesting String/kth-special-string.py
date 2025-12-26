d5edbbab60cfde3a3646dcb852292e629b91428b
정선재 <95292789+jaysj0226@users.noreply.github.com>
2025-12-26 14:17:03 +0900

    Title: The K-th Most Interesting String, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251226/The K-th Most Interesting String/kth-special-string.py
@@ -0,0 +1,22 @@
+n, k, t = input().split()
+n, k = int(n), int(k)
+str = [input() for _ in range(n)]
+
+# Please write your code here.
+def find_str(str):
+    arr = []
+    for i in range(n):
+        if t in str[i]:
+            arr.append(str[i])
+
+    return arr
+
+def sort_arr(arr):
+    arr = sorted(arr)
+    return arr
+
+str = find_str(str)
+str = sort_arr(str)
+res = str[k-1]
+
+print(res)
