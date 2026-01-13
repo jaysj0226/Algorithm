0e5d8bc05e7d50e0ff4a31a1d1bc74b2bf451f74
정선재 <95292789+jaysj0226@users.noreply.github.com>
2026-01-13 12:44:23 +0900

    Title: Maximum Overlapping Point, Time: 55ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260113/Maximum Overlapping Point/maximum-overlapped-points.py
@@ -0,0 +1,20 @@
+n = int(input())
+segments = [tuple(map(int, input().split())) for _ in range(n)]
+MAX_NUM = 1000
+
+# Please write your code here.
+# OFFSET 필요없겠네
+
+def lines(arr,segment):
+    first,last = segment
+    for i in range(first,last+1):
+        arr[i] += 1
+        
+
+def simulation():
+    arr = [0] * MAX_NUM
+    for segment in segments:
+        lines(arr,segment)
+    print(max(arr))
+
+simulation()
