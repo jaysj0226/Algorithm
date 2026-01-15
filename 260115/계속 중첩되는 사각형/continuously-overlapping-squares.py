cbf88d2dce721c6fc3c0d4716b74a3d1a58641d9
정선재 <95292789+jaysj0226@users.noreply.github.com>
2026-01-15 20:21:24 +0900

    Title: 계속 중첩되는 사각형, Time: 56ms, Memory: 17MB, Status: Passed - Codetree

--- 260115/계속 중첩되는 사각형/continuously-overlapping-squares.py
+++ 260115/계속 중첩되는 사각형/continuously-overlapping-squares.py
@@ -1,22 +1,22 @@
 n = int(input())
 OFFSET = 100
-MAX_N = 201
+MAX_R = 201
 # Please write your code here.
 lines = [tuple(map(int,input().split())) for _ in range(n)]
-areas = [[0]*MAX_N for _ in range(MAX_N)]
+areas = [[0]*MAX_R for _ in range(MAX_R)]
 
 def get_area(idx,line):
     x1,y1,x2,y2 = line
     x1 += OFFSET; y1 += OFFSET; x2 += OFFSET; y2 += OFFSET
 
     val = 2 if idx%2 else 1
-    for i in range(x1,x2):
-        for j in range(y1,y2):
-            areas[i][j]=val
+    for x in range(x1,x2):
+        for y in range(y1,y2):
+            areas[x][y]=val
 
 
 def print_ans():
-    print(sum(1 for area in areas for i in area if i == 2))
+    print(sum(i==2 for area in areas for i in area))
 
 # 처음에 빨, 다음 파 --> 0 : 빨, 1 : 파 --> 짝 : 빨, 홀 : 파
 def simulation():
