37f3a99139685f5f7a9eeef430d4ad3cefe06e3f
정선재 <95292789+jaysj0226@users.noreply.github.com>
2026-01-15 16:04:09 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 103ms, Memory: 47MB, Status: Passed - Codetree

---
+++ 260115/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -0,0 +1,45 @@
+OFFSET = 1000
+MAX_R = 2000 
+
+rects = 2
+lines = [tuple(map(int, input().split())) for _ in range(rects)]
+
+blocks = [[0] * (MAX_R + 1) for _ in range(MAX_R + 1)]
+
+def paint(idx, line):
+    x1, y1, x2, y2 = line
+
+    x1 += OFFSET
+    y1 += OFFSET
+    x2 += OFFSET
+    y2 += OFFSET
+
+    for x in range(x1, x2):
+        for y in range(y1, y2):
+            blocks[x][y] = idx 
+
+def bounding_area_for_first():
+    min_x, max_x = MAX_R, 0
+    min_y, max_y = MAX_R, 0
+    exist = False
+
+    for x in range(MAX_R + 1):
+        for y in range(MAX_R + 1):
+            if blocks[x][y] == 1:
+                exist = True
+                if x < min_x: min_x = x
+                if x > max_x: max_x = x
+                if y < min_y: min_y = y
+                if y > max_y: max_y = y
+
+    if not exist:
+        return 0
+    return (max_x - min_x + 1) * (max_y - min_y + 1)
+
+def simulation():
+    for idx, line in enumerate(lines, start=1):
+        paint(idx, line)
+
+    print(bounding_area_for_first())
+
+simulation()
