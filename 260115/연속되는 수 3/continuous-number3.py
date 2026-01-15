389034ea6e8a44d4ee638187a9865cbc3a972d7d
정선재 <95292789+jaysj0226@users.noreply.github.com>
2026-01-15 20:48:27 +0900

    Title: 연속되는 수 3, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260115/연속되는 수 3/continuous-number3.py
@@ -0,0 +1,20 @@
+N = int(input())
+arr = [int(input()) for _ in range(N)]
+
+# Please write your code here.
+def simulation():
+    best = 0
+    cnt = 0
+    for i in range(N-1):
+        if arr[i] > 0 and arr[i+1] > 0:
+            cnt += 1
+        elif arr[i] < 0 and arr[i+1] < 0:
+            cnt += 1
+        else:
+            cnt = 1
+        best = max(best,cnt)
+    
+    print(best)
+
+simulation()
+        
